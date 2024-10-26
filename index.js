8b14b3c67b3bf6ab983156bef38736440392b753
PRAVEEN200307 <crouselearn@gmail.com>
2024-10-26 13:00:44 +0530

    feat : added a cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import router from './routes/router.js';
 import { connectDb } from './Database/db.js';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
+app.use(cors());
 
 connectDb();
 
